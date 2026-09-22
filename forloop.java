ae815bae69b69506aeedc96047f1846a94c997e1
shilpayelti <shilpayeltiwar@gmail.com>
2026-09-22 11:18:26 -0700

    Add files via upload

---
+++ forloop.java
@@ -0,0 +1,14 @@
+package basicjavaprogram;
+
+public class forloop {
+
+	public static void main(String[] args) {
+		int sum = 10;
+		for(int i=1; i<=10;i++) {
+			sum = sum+i;
+			
+		}
+System.out.println("the sum of the number are :" +sum);
+	}
+
+}
